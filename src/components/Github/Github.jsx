920330e4e322d4bdc4bffd4e20f3ef1634d0068e
dev-prajwal <47482881+dev-prajwal@users.noreply.github.com>
2024-06-11 16:55:20 +0530

    final changes

---
+++ src/components/Github/Github.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { useLoaderData } from 'react-router-dom'
+
+function Github() {
+    const data = useLoaderData()
+    return (
+        <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>
+            Public Repos: {data.public_repos}
+            <img
+                src={data.avatar_url}
+                width="300"
+            />
+        </div>
+    )
+}
+
+export default Github
+
+export const githubInfoLoader = async() => {
+    const response = await fetch(`https://api.github.com/users/dev-prajwal`)
+    return response.json()
+}
